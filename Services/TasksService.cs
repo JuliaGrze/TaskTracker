723ad33e38010a843d7314da7eed57e35fcd574d
Julia Grzesiewicz <julia.grzesiewicz@op.pl>
2025-04-22 17:04:22 +0200

    Added AddTask method implemntation and appropriate test for this

---
+++ Services/TasksService.cs
@@ -0,0 +1,73 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+
+namespace Services
+{
+    public class TasksService : ITasksService
+    {
+        //private fields
+        private List<TaskEntity> _tasks;
+        public TasksService()
+        {
+            _tasks = new List<TaskEntity>();
+        }
+
+        public TaskResponse AddTask(TaskAddRequest? taskAddRequest)
+        {
+            //Validation: taskAddRequest can't be null
+            if (taskAddRequest == null)
+                throw new ArgumentNullException(nameof(taskAddRequest));
+
+            //Validation: title can't be null
+            if(String.IsNullOrEmpty(taskAddRequest.Title))
+                throw new ArgumentException(nameof(taskAddRequest.Title));
+
+            //Validation: title length can't be shorter than 3
+            if(taskAddRequest.Title.Length < 3)
+                throw new ArgumentException(nameof(TaskAddRequest.Title));
+
+            //convert TaskAddRequest into Task type
+            TaskEntity task = taskAddRequest.ToTask();
+
+            //generate TaskID
+            task.TaskID = Guid.NewGuid();
+
+            //Add Task Entity obkect into _tasks list
+            _tasks.Add(task);
+
+            return task.ToTaskResponse();
+        }
+
+        public bool DeleteTask(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TaskResponse> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TaskResponse> GetFilteredTasks(string searchBy, Taskstatus taskstatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TaskResponse> GetSortedTasks(List<TaskResponse> allTasks, string sortBy, SortOrderEnum sortOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskResponse? GetTaskById(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskResponse UpdateTask(TaskUpdateRequest? taskUpdateRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskTrackerTests/TasksServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace TaskTrackerTests
+{
+    public class TasksServiceTest
+    {
+        //fields
+        private readonly ITasksService _tasksService;
+        public TasksServiceTest()
+        {
+            _tasksService = new TasksService();
+        }
+
+        #region AddTask
+
+        //Test 1: when TaskAddRequest in null, should retrun ArgumentNullException
+        [Fact]
+        public void AddTask_NullTaskAddRequest()
+        {
+            //Arrange
+            TaskAddRequest? request = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            //act
+            _tasksService.AddTask(request));
+        }
+
+        //Test 2: When task title is empty, should return ArgumentException
+        [Fact]
+        public void AddTask_TitleIsNull()
+        {
+            //Arrange: task title is null
+            TaskAddRequest? taskAddRequest = new TaskAddRequest() { Title = null };
+
+            //Assert & act
+            Assert.Throws<ArgumentException>(() => _tasksService.AddTask(taskAddRequest));
+        }
+
+        //Test 3: When task title is shorter than 3 chars, it should return ArgumentException
+        [Fact]
+        public void AddTask_TitleIsTooShort()
+        {
+            //Arrange: task title is too short
+            TaskAddRequest? taskAddRequest = new TaskAddRequest() { Title = "Hi"};
+
+            //Assert & act
+            Assert.Throws<ArgumentException>(() => _tasksService.AddTask(taskAddRequest));
+        }
+
+        //Test 4: Valid data, it should insert task into task list and it should return an object of TaskResponse, which includes with the newly generated task id
+        [Fact]
+        public void AddTask_ProperData()
+        {
+            //Arrange: valid data
+            TaskAddRequest? taskAddRequest = new TaskAddRequest() { Title = "Running", Description = "Running 10 km" };
+
+            //Act
+            TaskResponse taskResponse = _tasksService.AddTask(taskAddRequest);
+
+            //Assert
+            Assert.True(taskResponse.TaskID != Guid.Empty);
+        }
+
+        #endregion
+    }
+}
