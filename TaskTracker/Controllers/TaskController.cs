115bf47a1aa779af67527cd2b352c9797dd8f2d3
Julia Grzesiewicz <julia.grzesiewicz@op.pl>
2025-04-23 09:42:39 +0200

    Added start configuration for Views

---
+++ TaskTracker/Controllers/TaskController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTracker.Controllers
+{
+    public class TaskController : Controller
+    {
+        [Route("tasks/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
